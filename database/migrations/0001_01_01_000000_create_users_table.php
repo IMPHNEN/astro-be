74dbe2a7aba09ae1fc20f49937b871d5f7fd075f
xirf <andikanp@proton.me>
2024-10-31 00:38:19 +0700

    feat: add user table and projects

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,17 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('slug')->unique()->default(hash('sha256', time())); // Stupid but it works :D;
+            $table->string('username')->unique();
+            $table->string('backgroud')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('website')->nullable();
+            $table->json('socials')->nullable();
+            $table->string('location')->nullable();
+            $table->string('timezone')->nullable();
+            $table->string('language')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('cv')->nullable();
+            $table->enum('role', ['user', 'investor', 'company', 'admin'])->default('user');
             $table->string('email')->unique();
+            $table->string('backup_email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -40,8 +55,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
